0681ddc0c202b876faf39687e6e9467a48409f82
Yevhenii <stupachenko30@gmail.com>
2026-08-11 19:45:45 +0200

    add 30.2.5

--- src/main/java/de/demowebshop/core/ApplicationManager.java
+++ src/main/java/de/demowebshop/core/ApplicationManager.java
@@ -5,19 +5,40 @@ import de.demowebshop.fw.UserHelper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class ApplicationManager{
 
+    String browser;
     WebDriver driver;
     UserHelper user;
     HomePageHelper homePage;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if (browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if (browser.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        else if (browser.equalsIgnoreCase("safari")) {
+            WebDriverManager.safaridriver().setup();
+            driver = new SafariDriver();
+        }
         user = new UserHelper(driver);
         homePage = new HomePageHelper(driver);
         driver.get("https://demowebshop.tricentis.com/");
--- src/test/java/de/demowebshop/core/TestBase.java
+++ src/test/java/de/demowebshop/core/TestBase.java
@@ -1,12 +1,14 @@
 package de.demowebshop.core;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager
+            (System.getProperty("browser", Browser.CHROME.browserName()));
 
     @BeforeMethod
     public void setUp() {
