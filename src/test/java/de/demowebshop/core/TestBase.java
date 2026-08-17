52accb0115d9c0f48c124a95f30ebda873bca496
Yevhenii <stupachenko30@gmail.com>
2026-08-17 14:25:24 +0200

    add 35.1

--- src/test/java/de/demowebshop/core/TestBase.java
+++ src/test/java/de/demowebshop/core/TestBase.java
@@ -1,14 +1,20 @@
 package de.demowebshop.core;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
 
     protected static ApplicationManager app = new ApplicationManager
             (System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeMethod
     public void setUp() {
@@ -20,5 +26,16 @@ public class TestBase {
         app.stop();
     }
 
-}
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test {}",method.getName() );
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
+    }}
+
+
+
 
